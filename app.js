40d2f66efbfde6a2717eb013e83ab5d4b0efbdf1
Hack Reactor Student <student@hackreactor.com>
2017-07-12 11:42:21 -0500

    Did the thing

--- app.js
+++ app.js
@@ -1,1 +1,65 @@
 // TODO
+//create GroceryList component
+  //contains unordered list of 2 grocery items
+  //render this to <div> with id of "app"
+
+
+var App = () => (
+  <div>
+    <h1>Grocery List</h1>
+    <GroceryList listItems = {['Cucumbers','Kale']} />
+  </div>
+);
+
+var GroceryList = (props) => (
+  <ul>
+    {props.listItems.map(item =>
+      <GroceryListItem item={item} />
+    )}
+  </ul>
+);
+
+// var Cucumbers = () => (
+//   <li>Cucumbers</li>
+// );
+
+// var Kale = () => (
+//   <li>Kale</li>
+// );
+
+// var GroceryListItem = (props) => (
+//   <li>{props.items}</li>
+// );
+
+class GroceryListItem extends React.Component {
+  
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      isHovering: false
+    };
+  }
+
+  onGroceryItemHover() {
+    this.setState({isHovering: !this.state.isHovering});
+  }
+
+    
+
+  render() {
+    var style = {
+      'fontWeight': this.state.isHovering ? 'bold' : 'normal'
+    }
+    
+    return (
+      <li style={style} onMouseEnter={this.onGroceryItemHover.bind(this)} onMouseLeave={this.onGroceryItemHover.bind(this)}>{this.props.item}</li>
+    );
+  }
+
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
+
+
+
